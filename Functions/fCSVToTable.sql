59c01ec5b1e6d2639aa8ad37651608c55e63de5f
Brian Ritchie <brian.ritchie@gmail.com>
2020-12-20 18:29:05 -0500

    Add fCSVToTable function

---
+++ Functions/fCSVToTable.sql
@@ -0,0 +1,22 @@
+CREATE FUNCTION [dbo].[fCSVToTable] ( @StringInput VARCHAR(MAX) )
+RETURNS @OutputTable TABLE ( [String] VARCHAR(100) )
+AS
+BEGIN
+
+    DECLARE @String    VARCHAR(100)
+
+    WHILE LEN(@StringInput) > 0
+    BEGIN
+        SET @String      = LEFT(@StringInput, 
+                                ISNULL(NULLIF(CHARINDEX(',', @StringInput) - 1, -1),
+                                LEN(@StringInput)))
+        SET @StringInput = SUBSTRING(@StringInput,
+                                     ISNULL(NULLIF(CHARINDEX(',', @StringInput), 0),
+                                     LEN(@StringInput)) + 1, LEN(@StringInput))
+
+        INSERT INTO @OutputTable ( [String] )
+        VALUES ( @String )
+    END
+    
+    RETURN
+END
